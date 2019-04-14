b3717b68d615c1c9d24d340a0095da61455f355a
tuttlepower <tatuttle2015@gmail.com>
2019-04-14 19:33:15 -0400

    Creation

---
+++ pageCreator.py
@@ -0,0 +1,43 @@
+#pageCreator
+
+def webCreator(pageName):
+    # write-html.py
+    fileName= pageName
+
+    f = open(fileName +'.html','w')
+
+    #opens the head, which stores metadata, links to css and others
+    g = open('HTML/head.txt','r')
+
+    #opens the header, which will include logo and top bar (content)
+    h = open(pageName+'/'+pageName+'Header.txt','r')
+
+    #opens the body
+    i = open(pageName+'/'+pageName+'Body.txt','r')
+
+    #opens the footer, which will hold scripts and close out the html
+    j = open('HTML/Footer.txt','r')
+
+    #sets a string named head equal to all of the information in g, which is the FacehuggerHead
+    head =(g.read())
+
+    #sets a string named header equal to all of the information in h, which is the FacehuggerHeader
+    header =(h.read())
+
+    #Sets the body to a string
+    body =(i.read())
+
+    #sets a string named footer equal to all of the information in i, which is the FacehuggerFooter
+    footer = (j.read())
+
+    message = head+header+body+footer
+
+    f.write(message)
+    f.close()
+
+webCreator("index")
+webCreator("Economics")
+webCreator("Design")
+webCreator("Politics")
+webCreator("Programming")
+webCreator("Timeline")
