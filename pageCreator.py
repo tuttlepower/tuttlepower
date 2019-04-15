f288cf075538033678c2c1653480abde05a0ef03
tuttlepower <tatuttle2015@gmail.com>
2019-04-14 21:41:23 -0400

    Creating a Summary every time is difficult

--- pageCreator.py
+++ pageCreator.py
@@ -18,16 +18,16 @@ def webCreator(pageName):
     #opens the footer, which will hold scripts and close out the html
     j = open('HTML/Footer.txt','r')
 
-    #sets a string named head equal to all of the information in g, which is the FacehuggerHead
+    #sets a string named head equal to all of the information in g, which is the Head
     head =(g.read())
 
-    #sets a string named header equal to all of the information in h, which is the FacehuggerHeader
+    #sets a string named header equal to all of the information in h, which is the Header
     header =(h.read())
 
     #Sets the body to a string
     body =(i.read())
 
-    #sets a string named footer equal to all of the information in i, which is the FacehuggerFooter
+    #sets a string named footer equal to all of the information in i, which is the Footer
     footer = (j.read())
 
     message = head+header+body+footer
