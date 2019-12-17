d4b6cb82f2dde8a9a7334e4723c7783ed40ba2c7
tuttlepower <tatuttle2015@gmail.com>
2019-12-17 18:12:08 -0500

    footer edit

--- pageCreator.py
+++ pageCreator.py
@@ -4,19 +4,19 @@ def webCreator(pageName):
     # write-html.py
     fileName= pageName
 
-    f = open('tuttlepower/' + fileName +'.html','w')
+    f = open( fileName +'.html','w')
 
     #opens the head, which stores metadata, links to css and others
-    g = open('tuttlepower/HTML/head.txt','r')
+    g = open('HTML\head.txt','r') 
 
     #opens the header, which will include logo and top bar (content)
-    h = open('tuttlepower/' + pageName+'/'+pageName+'Header.txt','r')
+    h = open( pageName+'/'+pageName+'Header.txt','r')
 
     #opens the body
-    i = open('tuttlepower/' + pageName+'/'+pageName+'Body.txt','r')
+    i = open( pageName+'/'+pageName+'Body.txt','r')
 
     #opens the footer, which will hold scripts and close out the html
-    j = open('tuttlepower/HTML/Footer.txt','r')
+    j = open('HTML/Footer.txt','r')
 
     #sets a string named head equal to all of the information in g, which is the Head
     head =(g.read())
