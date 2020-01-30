0f7040db2a01665f0f6b95b0aab4a2fcc1cc805e
tuttlepower <tatuttle2015@gmail.com>
2020-01-30 13:02:01 -0500

    Graph fixes
    Fixed the graphing equation, smoothing it out. Still only Federal taxes

--- Resources/Projects/taxes.py
+++ Resources/Projects/taxes.py
@@ -14,19 +14,19 @@ while y < 1000000:
     x.append(y)
     income.append(y)
     if y >= 0 and y<=9700:
-        taxes.append(0)
+        taxes.append(y*.1)
     if y > 9700 and y<=39475:
-        taxes.append(970)
+        taxes.append(970 + ((y-9700)*.12))
     if y > 39475 and y<=84200:
-        taxes.append(4543.00)
+        taxes.append(4543.00+((y-39475)*.22))
     if y > 84200 and y<=160725:
-        taxes.append(14382.50)
+        taxes.append(14382.50+((y-84200)*.24))
     if y > 160725 and y<=204100:
-        taxes.append(32748.50)
+        taxes.append(32748.50+((y-160725)*.32))
     if y > 204100 and y<=510300:
-        taxes.append(46628.50)
+        taxes.append(46628.50+((y-204100)*.35))
     if y > 510300:
-        taxes.append(153798.50)
+        taxes.append(153798.50+((y-510300)*.37))
 
     y=y+1
 
