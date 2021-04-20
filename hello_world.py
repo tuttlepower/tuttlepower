8e4447be24dbf92b08f43d95f167d024d4946ee5
Travis Tuttle <tatuttle2015@gmail.com>
2021-04-20 18:25:20 -0400

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -5,10 +5,10 @@ import os
 #sets up the api to play with Twitter
 def py_twit_setup():
   # Retreive secrets
-  CONSUMER_KEY = os.environ.get['CONSUMER_KEY']
-  CONSUMER_SECRET = os.environ.get['CONSUMER_SECRET']
-  ACCESS_TOKEN = os.environ.get['ACCESS_TOKEN']
-  ACCESS_TOKEN_SECRET = os.environ.get['ACCESS_TOKEN_SECRET']
+  CONSUMER_KEY = os.environ['CONSUMER_KEY']
+  CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+  ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+  ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
   # passes unique identification into an authorization to use tweepy
   auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
   auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
