16104fa5709e722d539ba6d764003c0752df847b
Travis Tuttle <tatuttle2015@gmail.com>
2021-04-20 18:00:21 -0400

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,11 @@
+# import libraries
 import tweepy
 import os
-print('Hello World!')
-print(os.environ)
+
+# Retreive secrets
+CONSUMER_KEY = os.environ.get['CONSUMER_KEY']
+CONSUMER_SECRET = os.environ.get['CONSUMER_SECRET']
+ACCESS_TOKEN = os.environ.get['ACCESS_TOKEN']
+ACCESS_TOKEN_SECRET = os.environ.get['ACCESS_TOKEN_SECRET']
+
+print(CONSUMER_KEY)
